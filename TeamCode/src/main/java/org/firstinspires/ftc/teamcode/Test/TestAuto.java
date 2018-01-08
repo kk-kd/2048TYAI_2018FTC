a533bb86f0f2bb3171507361882f01f3f6886708
CandyZQ <candyzhouziqing@hotmail.com>
2018-01-08 13:44:51 +0800

    TestAuto for Auto concept added

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test/TestAuto.java
@@ -1,17 +1,15 @@
 package org.firstinspires.ftc.teamcode.Test;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.Servo;
 
-import org.firstinspires.ftc.teamcode.BaseClasses.NormalDrive;
 import org.firstinspires.ftc.teamcode.BaseClasses.OmniDrive;
 import org.firstinspires.ftc.teamcode.BaseClasses.VuforiaUsage;
 
 /**
  * Created by candy on 27/09/2017.
  */
-@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="MainOp", group="main")
-public class TestTeleOp extends LinearOpMode{
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name="Aut", group="main")
+public class TestAuto extends LinearOpMode{
 
     OmniDrive robot = new OmniDrive();
     VuforiaUsage nav = new VuforiaUsage();
@@ -25,8 +23,14 @@ public class TestTeleOp extends LinearOpMode{
         nav.activate();
 
         while(opModeIsActive()){
-            nav.startIdentifying();
-            robot.manualDrive();
+            telemetry.addData("1","1");
+            telemetry.update();
+            robot.moveRobot(0,1,0);
+            sleep(3000);
+            telemetry.addData("2",2);
+            telemetry.update();
+            robot.moveRobot(0,0,0);
+            sleep(3000);
 
         }
     }
