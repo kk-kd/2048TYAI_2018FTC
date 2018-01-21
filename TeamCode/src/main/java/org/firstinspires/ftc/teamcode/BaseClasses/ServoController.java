c2125e3d32851e61ba5bbeea5c1d5bb7a306221e
CandyZQ <candyzhouziqing@hotmail.com>
2018-01-21 17:37:56 +0800

    Servocontroller created for servo operations

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseClasses/ServoController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseClasses/ServoController.java
@@ -1,8 +1,53 @@
 package org.firstinspires.ftc.teamcode.BaseClasses;
 
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
 /**
  * Created by candy on 20/11/2017.
  */
 
 public class ServoController {
+
+    private LinearOpMode opMode;
+
+    private Servo  servoA      = null;
+    private Servo  servoB      = null;
+    private Servo  servoC      = null;
+
+    /* Constructor */
+    public ServoController(){
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(LinearOpMode opMode) {
+
+        this.opMode = opMode;
+
+        servoA = this.opMode.hardwareMap.get(Servo.class, "ServoA");
+        servoB = this.opMode.hardwareMap.get(Servo.class, "ServoB");
+        servoC = this.opMode.hardwareMap.get(Servo.class, "ServoC");
+
+        setPosition(0);
+
+        this.opMode.telemetry.addData(">","Press start");
+        this.opMode.telemetry.update();
+    }
+
+    public void setPosition(double position){
+        servoA.setPosition(position);
+        servoB.setPosition(position);
+        servoC.setPosition(position);
+    }
+
+    public void setServoA(double postionA) {
+        servoA.setPosition(postionA);
+    }
+    public void setServoB(double postionB) {
+        servoA.setPosition(postionB);
+    }
+    public void setServoC(double postionC) {
+        servoA.setPosition(postionC);
+    }
 }
